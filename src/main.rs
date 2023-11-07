ddb001d95ae95e539b0168eb6b471d235d21a3d3
eneoli <oliver@eneoli.de>
2023-11-07 22:53:09 +0100

    test

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,3 @@
-use flakeshot::Error;
-
-fn run() ->Result<(), Error> {
-    Err(Error::NotImplemented)
-}
-
 fn main() {
-
-    let result = run();
-
-    if let Err(err) = result {
-        match err {
-        }
-    }
+   println!("TEST");
 }
