e8de9c5023a1c1493d4a80797e515f54b689cfc3
TornaxO7 <tornax@proton.me>
2023-11-11 01:31:49 +0100

    flake: adding `apps` and `overlay` to flake

--- flake.nix
+++ flake.nix
@@ -13,17 +13,51 @@
           (system: function (import nixpkgs {
             inherit system;
 
-            overlays = [ (import rust-overlay) ];
+            overlays = [ rust-overlay.overlays.default ];
           }));
+
+      mkFlakeshot = { rustPlatform, lib, ... }: rustPlatform.buildRustPackage {
+        pname = "flakeshot";
+        version = "0.0.1";
+
+        src = ./.;
+        cargoLock.lockFile = ./Cargo.lock;
+
+        cargoSha256 = "";
+
+        meta = {
+          description = "A screenshot tool for wayland and xorg!";
+          homepage = "https://github.com/eneoli/flakeshot/";
+          license = lib.licenses.mit;
+        };
+      };
     in
     {
+      apps = forAllSystems
+        (pkgs:
+          let
+            flakeshotPkg = pkgs.callPackage mkFlakeshot { };
+          in
+          rec {
+            default = flakeshot;
+
+            flakeshot = {
+              type = "app";
+              program = "${flakeshotPkg}/bin/flakeshot";
+            };
+          });
+
+      overlays.default = final: prev: {
+        flameshot = prev.callPackage mkFlakeshot { };
+      };
+
       devShells = forAllSystems (pkgs: {
         default =
           let
             toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
           in
           pkgs.mkShell rec {
-            packages = with pkgs; [
+            packages = [
               toolchain
             ];
 
@@ -36,8 +70,9 @@
               dbus
             ];
 
-            LD_LIBRARY_PATH = "$LD_LIBRARY_PATH:${builtins.toString (pkgs.lib.makeLibraryPath buildInputs)}";
+            LD_LIBRARY_PATH = "$LD_LIBRARY_PATH:${builtins.toString (pkgs.lib.makeLibraryPath buildInputs)} ";
           };
       });
     };
 }
+
