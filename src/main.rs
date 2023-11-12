6f278ab30b7b00e760d41f98b71cf592d75e9fea
eneoli <oliver@eneoli.de>
2023-11-12 01:02:08 +0100

    WIP: wayland screenshots work

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 use std::fmt::Debug;
+use std::fs::File;
+use std::os::fd::{AsFd};
+use std::path::Path;
+use std::thread::sleep;
+use std::time::Duration;
+use memmap2::MmapMut;
 use wayland_client::{protocol::wl_registry, Connection, Dispatch, QueueHandle, Proxy};
-use wayland_client::protocol::{wl_output, wl_shm};
+use wayland_client::protocol::{wl_buffer, wl_output, wl_shm, wl_shm_pool};
 use wayland_client::protocol::wl_output::WlOutput;
 use wayland_client::protocol::wl_registry::WlRegistry;
 use wayland_client::protocol::wl_shm::WlShm;
@@ -10,6 +16,9 @@ use wayland_protocols_wlr::screencopy::v1::client::zwlr_screencopy_manager_v1::Z
 use flakeshot::backend::geometry::Geometry;
 use flakeshot::backend::output_info::OutputInfo;
 use flakeshot::backend::output_mode::OutputMode;
+use wayland_client::protocol::wl_buffer::WlBuffer;
+use wayland_client::protocol::wl_shm::Format::{Xbgr8888};
+use wayland_client::protocol::wl_shm_pool::WlShmPool;
 
 const WL_SHM: &'static str = "wl_shm";
 
@@ -23,7 +32,26 @@ struct AppData {
     wl_output: Option<WlOutput>,
     zwlr_screencopy_manager_v1: Option<ZwlrScreencopyManagerV1>,
 
-    outputs:Vec<OutputInfo>,
+    outputs: Vec<OutputInfo>,
+
+    ready: bool,
+}
+
+fn create_shm_file(name: &'static str, bytes: u64) -> anyhow::Result<File> {
+    let options = memfd::MemfdOptions::default()
+        .allow_sealing(true);
+
+    let mfd = options.create(name)?;
+
+    mfd.as_file().set_len(bytes)?;
+
+    mfd.add_seals(&[
+        memfd::FileSeal::SealShrink,
+        memfd::FileSeal::SealGrow,
+        memfd::FileSeal::SealSeal,
+    ])?;
+
+    Ok(mfd.into_file())
 }
 
 impl Dispatch<wl_registry::WlRegistry, ()> for AppData {
@@ -35,6 +63,9 @@ impl Dispatch<wl_registry::WlRegistry, ()> for AppData {
              qhandle: &QueueHandle<Self>) {
         if let wl_registry::Event::Global { name, interface, version } = event {
             println!("[{}] {} (v{})", name, interface, version);
+
+            //println!("{:#?}",interface);
+
 
             match interface.as_str() {
                 WL_SHM => { state.wl_shm = Some(state.registry.bind::<WlShm, _, _>(name, version, qhandle, ())) }
@@ -53,8 +84,31 @@ impl Dispatch<WlShm, ()> for AppData {
              data: &(),
              conn: &Connection,
              qhandle: &QueueHandle<Self>) {
-        println!("SHM");
+
+
+        //println!("Advertised Format for SHM");
+        //println!("{:#?}", event);
     }
+}
+
+impl Dispatch<WlShmPool, ()> for AppData {
+    fn event(state: &mut Self,
+             proxy: &WlShmPool,
+             event: wl_shm_pool::Event,
+             data: &(),
+             conn: &Connection,
+             qhandle: &QueueHandle<Self>,
+    ) {}
+}
+
+impl Dispatch<WlBuffer, ()> for AppData {
+    fn event(state: &mut Self,
+             proxy: &WlBuffer,
+             event: wl_buffer::Event,
+             data: &(),
+             conn: &Connection,
+             qhandle: &QueueHandle<Self>,
+    ) {}
 }
 
 impl Dispatch<ZwlrScreencopyManagerV1, ()> for AppData {
@@ -63,9 +117,7 @@ impl Dispatch<ZwlrScreencopyManagerV1, ()> for AppData {
              event: zwlr_screencopy_manager_v1::Event,
              data: &(),
              conn: &Connection,
-             qhandle: &QueueHandle<Self>) {
-        println!("Screencopy Manager");
-    }
+             qhandle: &QueueHandle<Self>) {}
 }
 
 impl Dispatch<ZwlrScreencopyFrameV1, ()> for AppData {
@@ -75,7 +127,22 @@ impl Dispatch<ZwlrScreencopyFrameV1, ()> for AppData {
              data: &(),
              conn: &Connection,
              qhandle: &QueueHandle<Self>) {
-        println!("Something happened");
+        println!("Buffer!!!");
+        if let zwlr_screencopy_frame_v1::Event::BufferDone = event {
+            state.ready = true;
+        }
+
+        if let zwlr_screencopy_frame_v1::Event::Failed = event {
+            println!("FAILED!");
+        }
+
+        if let zwlr_screencopy_frame_v1::Event::Buffer { width, .. } = event {
+            println!("{}", width);
+        }
+
+        if let zwlr_screencopy_frame_v1::Event::Ready { .. } = event {
+            println!("Buffer ready");
+        }
     }
 }
 
@@ -85,29 +152,28 @@ impl Dispatch<WlOutput, ()> for AppData {
              event: wl_output::Event,
              data: &(),
              conn: &Connection,
-             qhandle: &QueueHandle<Self>
+             qhandle: &QueueHandle<Self>,
     ) {
-
-        let mut outputQuery = state.outputs
+        let mut output_query = state.outputs
             .iter_mut()
-            .find(|output| output.output.id() == proxy.id() );
+            .find(|output| output.output.id() == proxy.id());
 
-        let output: &mut OutputInfo = match outputQuery {
+        let output = match output_query {
             Some(output) => output,
             None => {
                 let mut output_info = OutputInfo::from_wl_output(proxy.clone());
                 state.outputs.push(output_info);
 
                 state.outputs.last_mut().unwrap()
-            },
+            }
         };
 
         match event {
-            wl_output::Event::Name {name} => output.name = name,
-            wl_output::Event::Description {description} => output.description = description,
-            wl_output::Event::Scale {factor} => output.scale = factor,
-            wl_output::Event::Geometry {..} => output.geometry = Geometry::from_wayland_geometry(event).unwrap(),
-            wl_output::Event::Mode {..} => output.mode = OutputMode::from_wayland_event(event).unwrap(),
+            wl_output::Event::Name { name } => output.name = name,
+            wl_output::Event::Description { description } => output.description = description,
+            wl_output::Event::Scale { factor } => output.scale = factor,
+            wl_output::Event::Geometry { .. } => output.geometry = Geometry::from_wayland_geometry(event).unwrap(),
+            wl_output::Event::Mode { .. } => output.mode = OutputMode::from_wayland_event(event).unwrap(),
             default => (),
         }
     }
@@ -128,21 +194,57 @@ fn main() {
         wl_shm: None,
         zwlr_screencopy_manager_v1: None,
         outputs: vec![],
+        ready: false,
     };
 
     queue.roundtrip(&mut appData).unwrap();
 
-    //appData.zwlr_screencopy_manager_v1.unwrap().
+    // setup shm
+    let mut memfile = create_shm_file("/tmp/flakeshot0000", 2160 * 3840 * 4).unwrap();
+    let shm_pool = appData.wl_shm.as_ref().unwrap().create_pool(memfile.as_fd(), 3840 * 2160 * 4, &queue_handle, ());
 
-    appData.zwlr_screencopy_manager_v1.clone().unwrap().capture_output(
-        0,
-        &appData.wl_output.clone().unwrap(),
-        &queue_handle,
-        (),
-    );
+    // TODO ka ob format richtig
+    let buffer: WlBuffer = shm_pool.create_buffer(0, 3840, 2160, 3840 * 4, Xbgr8888, &queue_handle, ());
 
-    queue.blocking_dispatch(&mut appData);
+    loop {
+        queue.blocking_dispatch(&mut appData).unwrap();
 
-    println!("{:#?}", appData.outputs);
+        if appData.outputs.len() > 0 {
 
+            //println!("SCREENSHOT!");
+
+            println!("{:#?}", appData.outputs.last_mut().unwrap().description);
+
+            let frame = appData.zwlr_screencopy_manager_v1
+                .as_ref()
+                .unwrap()
+                .capture_output(
+                    0,
+                    &appData.outputs.last_mut().unwrap().output,
+                    &queue_handle,
+                    (),
+                );
+
+            frame.copy(&buffer);
+
+            while (!appData.ready) {
+                queue.blocking_dispatch(&mut appData).unwrap();
+            }
+
+            sleep(Duration::from_secs(5));
+
+            let mut frame_mmap = unsafe { MmapMut::map_mut(&memfile).unwrap() };
+            let data = &mut *frame_mmap;
+
+            // println!("{:#?}", data.to_vec());
+
+            image::save_buffer(&Path::new("/home/oliver/screen.png"), data, 3840, 2160, image::ColorType::Rgba8).expect("TODO: panic message");
+
+            //println!("{:#?}", frame);
+
+            break;
+        }
+    }
+
+    //println!("{:#?}", appData.outputs);
 }
