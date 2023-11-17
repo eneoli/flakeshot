6431f0787a3d624acec301b6fe4d95e8f01778ca
eneoli <oliver@eneoli.de>
2023-11-17 02:52:30 +0100

    added errors

--- src/backend/wayland/wayland_error.rs
+++ src/backend/wayland/wayland_error.rs
@@ -7,4 +7,16 @@ pub enum WaylandError {
 
     #[error("Compositor did not provide a screenshot manager")]
     NoScreenshotManager,
+
+    #[error("Event Queue did not receive desired event")]
+    EventQueueTimeout,
+
+    #[error("We tried to access an uninitialized property. This is likely a bug")]
+    BrokenState,
+
+    #[error("The Wayland Compositor did not provide a format for the screenshot it made")]
+    MissingFormat,
+
+    #[error("We failed to convert the screenshot buffer into a image")]
+    ConvertImageFailed,
 }
