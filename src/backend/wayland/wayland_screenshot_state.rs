c1409619a2614e4a1ecfca3ad3c78709876b020e
eneoli <oliver@eneoli.de>
2023-11-17 03:07:48 +0100

    cleanup

--- src/backend/wayland/wayland_screenshot_state.rs
+++ src/backend/wayland/wayland_screenshot_state.rs
@@ -1,17 +1,17 @@
 use wayland_client::{Connection, Dispatch, Proxy, QueueHandle};
 use wayland_client::protocol::{wl_buffer, wl_output, wl_registry, wl_shm, wl_shm_pool};
 use wayland_client::protocol::wl_buffer::WlBuffer;
-use wayland_client::protocol::wl_output::WlOutput;
+use wayland_client::protocol::wl_output::{WlOutput};
 use wayland_client::protocol::wl_shm::{Format, WlShm};
 use wayland_client::protocol::wl_shm_pool::WlShmPool;
 use wayland_client::WEnum;
 use wayland_protocols_wlr::screencopy::v1::client::zwlr_screencopy_frame_v1::ZwlrScreencopyFrameV1;
 use wayland_protocols_wlr::screencopy::v1::client::{zwlr_screencopy_frame_v1, zwlr_screencopy_manager_v1};
 use wayland_protocols_wlr::screencopy::v1::client::zwlr_screencopy_manager_v1::ZwlrScreencopyManagerV1;
-use crate::backend::frame_meta::FrameMeta;
-use crate::backend::geometry::Geometry;
-use crate::backend::output_info::OutputInfo;
-use crate::backend::output_mode::OutputMode;
+use crate::backend::wayland::wayland_frame_meta::WaylandFrameMeta;
+use crate::backend::wayland::wayland_geometry::WaylandGeometry;
+use crate::backend::wayland::wayland_output_info::WaylandOutputInfo;
+use crate::backend::wayland::wayland_output_mode::WaylandOutputMode;
 
 /**
  *   Globals as advertised by the Wayland compositor.
@@ -23,16 +23,26 @@ const ZWLR_SCREENCOPY_MANAGER_V1: &'static str = "zwlr_screencopy_manager_v1";
 #[derive(Default, Debug)]
 pub struct WaylandScreenshotState {
     pub outputs_fetched: bool,
+    pub outputs: Vec<WaylandOutputInfo>,
+
+    // per screenshot
     pub screenshot_ready: bool,
-
-    pub outputs: Vec<OutputInfo>,
-
-    pub current_frame: Option<FrameMeta>,
+    pub current_frame: Option<WaylandFrameMeta>,
 
     pub wl_shm: Option<WlShm>,
     pub shm_formats: Vec<Format>,
 
     pub zwlr_screencopy_manager_v1: Option<ZwlrScreencopyManagerV1>,
+}
+
+impl WaylandScreenshotState {
+    /**
+     * Resets state to before a screenshot was made.
+     */
+    pub fn next_screen(self: &mut Self) {
+        self.screenshot_ready = false;
+        self.current_frame = None;
+    }
 }
 
 impl Dispatch<WlOutput, ()> for WaylandScreenshotState {
@@ -51,7 +61,7 @@ impl Dispatch<WlOutput, ()> for WaylandScreenshotState {
         let output = match output_query {
             Some(output) => output,
             None => {
-                let output_info = OutputInfo::from_wl_output(proxy.clone());
+                let output_info = WaylandOutputInfo::from_wl_output(proxy.clone());
                 state.outputs.push(output_info);
 
                 state.outputs.last_mut().unwrap()
@@ -62,8 +72,8 @@ impl Dispatch<WlOutput, ()> for WaylandScreenshotState {
             wl_output::Event::Name { name } => output.name = name,
             wl_output::Event::Description { description } => output.description = description,
             wl_output::Event::Scale { factor } => output.scale = factor,
-            wl_output::Event::Geometry { .. } => output.geometry = Geometry::from_wayland_geometry(event).unwrap(),
-            wl_output::Event::Mode { .. } => output.mode = OutputMode::from_wayland_event(event).unwrap(),
+            wl_output::Event::Geometry { .. } => output.geometry = WaylandGeometry::from_wayland_geometry(event).unwrap(),
+            wl_output::Event::Mode { .. } => output.mode = WaylandOutputMode::from_wayland_event(event).unwrap(),
             wl_output::Event::Done => state.outputs_fetched = true,
             _ => (),
         }
@@ -111,6 +121,7 @@ impl Dispatch<WlShm, ()> for WaylandScreenshotState {
         _conn: &Connection,
         _qhandle: &QueueHandle<Self>,
     ) {
+        // We got a supported format from the Wayland Compositor
         if let wl_shm::Event::Format { format } = event {
             if let WEnum::Value(format) = format {
                 state.shm_formats.push(format.clone());
@@ -129,15 +140,15 @@ impl Dispatch<ZwlrScreencopyFrameV1, ()> for WaylandScreenshotState {
         _qhandle: &QueueHandle<Self>,
     ) {
         if let zwlr_screencopy_frame_v1::Event::BufferDone = event {
-            // Not interested
-        }
-
-        if let zwlr_screencopy_frame_v1::Event::Buffer { .. } = event {
-            state.current_frame = FrameMeta::from_wayland_event(&event);
+            // Not interested, screenshot is ready on ready event.
         }
 
         if let zwlr_screencopy_frame_v1::Event::Ready { .. } = event {
             state.screenshot_ready = true;
+        }
+
+        if let zwlr_screencopy_frame_v1::Event::Buffer { .. } = event {
+            state.current_frame = WaylandFrameMeta::from_wayland_event(&event);
         }
 
         if let zwlr_screencopy_frame_v1::Event::Failed = event {
@@ -187,8 +198,8 @@ impl Dispatch<ZwlrScreencopyManagerV1, ()> for WaylandScreenshotState {
 
 // # Utility functions to create wrapper types
 
-impl Geometry {
-    pub fn from_wayland_geometry(event: wl_output::Event) -> Option<Geometry> {
+impl WaylandGeometry {
+    pub fn from_wayland_geometry(event: wl_output::Event) -> Option<WaylandGeometry> {
         if let wl_output::Event::Geometry {
             x,
             y,
@@ -196,16 +207,25 @@ impl Geometry {
             physical_height,
             model,
             make,
-            ..
+            subpixel,
+            transform
         } = event {
             Some(
-                Geometry {
+                WaylandGeometry {
                     x,
                     y,
                     physical_width,
                     physical_height,
                     model,
                     make,
+                    subpixel: match subpixel {
+                        WEnum::Value(subpixel) => subpixel,
+                        WEnum::Unknown(_) => None,
+                    },
+                    transform: match transform {
+                        WEnum::Value(transform) => transform,
+                        WEnum::Unknown(_) => None,
+                    },
                 }
             )
         } else {
@@ -214,19 +234,23 @@ impl Geometry {
     }
 }
 
-impl OutputMode {
+impl WaylandOutputMode {
     pub fn from_wayland_event(event: wl_output::Event) -> Option<Self> {
         if let wl_output::Event::Mode {
             height,
             width,
             refresh,
-            ..
+            flags,
         } = event {
             Some(
                 Self {
                     height,
                     width,
                     refresh,
+                    flags: match flags {
+                        WEnum::Value(flags) => flags,
+                        WEnum::Unknown(_) => None,
+                    },
                 }
             )
         } else {
@@ -235,7 +259,7 @@ impl OutputMode {
     }
 }
 
-impl FrameMeta {
+impl WaylandFrameMeta {
     pub fn from_wayland_event(event: &zwlr_screencopy_frame_v1::Event) -> Option<Self> {
         if let &zwlr_screencopy_frame_v1::Event::Buffer {
             format,
