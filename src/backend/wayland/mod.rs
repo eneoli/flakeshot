e713500653da666b310176e4cd0bdf6fe51524af
eneoli <oliver@eneoli.de>
2023-11-20 02:39:24 +0100

    using WaylandScreenshotManager

--- src/backend/wayland/mod.rs
+++ src/backend/wayland/mod.rs
@@ -1,14 +1,10 @@
 use crate::backend::wayland::wayland_error::WaylandError;
-use crate::backend::wayland::wayland_screenshot_state::WaylandScreenshotState;
-use crate::backend::wayland::wayland_shared_memory::WaylandSharedMemory;
 use crate::backend::OutputInfo;
 use image::DynamicImage::{ImageRgb8, ImageRgba8};
 use image::{DynamicImage, RgbImage, RgbaImage};
 use std::io::Read;
-use std::sync::{Arc, Mutex};
-use std::time::Duration;
 use wayland_client::protocol::wl_shm::Format;
-use wayland_client::{Connection, DispatchError, EventQueue, QueueHandle};
+use crate::backend::wayland::wayland_screenshot_manager::WaylandScreenshotManager;
 
 pub mod wayland_error;
 pub(crate) mod wayland_frame_meta;
@@ -17,6 +13,7 @@ pub(crate) mod wayland_output_info;
 pub(crate) mod wayland_output_mode;
 pub(crate) mod wayland_screenshot_state;
 pub(crate) mod wayland_shared_memory;
+pub(crate) mod wayland_screenshot_manager;
 
 /// The main function of this module.
 ///
@@ -42,96 +39,29 @@ pub(crate) mod wayland_shared_memory;
 /// }
 /// ```
 pub async fn create_screenshots() -> anyhow::Result<Vec<(OutputInfo, DynamicImage)>> {
-    let state_mutex = Arc::new(Mutex::new(WaylandScreenshotState::default()));
+    let mut manager = WaylandScreenshotManager::new()?;
+    let queue_handle = manager.get_queue_handle();
 
-    let (queue_mutex, queue_handle) = init_queue()?;
+    let screenshot_manager = manager.get_zwlr_screencopy_manager_v1()?.clone();
 
-    {
-        let state = &mut *state_mutex.lock().unwrap();
-        let queue = &mut *queue_mutex.lock().unwrap();
-
-        queue.roundtrip(state)?;
-    }
-
-    // spin util we got all outputs from wayland
-    await_queue_events(
-        |state| state.outputs_fetched,
-        queue_mutex.clone(),
-        state_mutex.clone(),
-    )
-        .await?;
-
-    let screenshot_manager = {
-        let state = &mut *state_mutex.lock().unwrap();
-
-        state
-            .zwlr_screencopy_manager_v1
-            .as_ref()
-            .ok_or(WaylandError::NoScreenshotManager)?
-            .clone()
-    };
-
-    let num_of_outputs = {
-        let state = &mut *state_mutex.lock().unwrap();
-        state.outputs.len()
+    let num_outputs = {
+        manager.get_outputs()?.len()
     };
 
     let mut screenshots: Vec<(OutputInfo, DynamicImage)> = vec![];
-    for i in 0..num_of_outputs {
-        let output = {
-            let state = &mut *state_mutex.lock().unwrap();
-            &state.outputs[i].clone()
+    for i in 0..num_outputs {
+        let frame = {
+            let output = &manager.get_outputs()?[i];
+
+            screenshot_manager
+                .capture_output(0, &output.output, &queue_handle, ())
         };
 
-        let frame = screenshot_manager.capture_output(0, &output.output, &queue_handle, ());
-
-        // spin until screenshot ready
-        await_queue_events(
-            |state| state.current_frame.is_some(),
-            queue_mutex.clone(),
-            state_mutex.clone(),
-        )
-            .await?;
-
-        let (width, height, stride, format) = {
-            let state = &mut *state_mutex.lock().unwrap();
-
-            let current_frame = state
-                .current_frame
-                .as_ref()
-                .ok_or(WaylandError::BrokenState("current_frame"))?;
-
-            let format = current_frame.format.ok_or(WaylandError::MissingFormat)?;
-
-            let width = current_frame.width;
-            let height = current_frame.height;
-            let stride = current_frame.stride;
-
-            (width, height, stride, format)
-        };
-
-        let mut shared_memory = {
-            let state = &mut *state_mutex.lock().unwrap();
-
-            WaylandSharedMemory::new(
-                state.wl_shm.as_ref().ok_or(WaylandError::NoShmBind)?,
-                &queue_handle,
-                width,
-                height,
-                stride,
-                format,
-            )?
-        };
+        let mut shared_memory = manager.create_shared_memory()?;
 
         frame.copy(shared_memory.get_buffer());
 
-        // spin until screenshot copied into shared buffer
-        await_queue_events(
-            |state| state.screenshot_ready,
-            queue_mutex.clone(),
-            state_mutex.clone(),
-        )
-            .await?;
+        manager.await_screenshot()?;
 
         // read from shared memory
         // data holds our screenshot
@@ -139,40 +69,24 @@ pub async fn create_screenshots() -> anyhow::Result<Vec<(OutputInfo, DynamicImag
         shared_memory.get_memfile().read_to_end(&mut data)?;
 
         let output_info = OutputInfo::try_from({
-            let state = &mut *state_mutex.lock().unwrap();
-            &state.outputs[i].clone()
+            &manager.get_outputs()?[i]
         })?;
-        let img = image_from_wayland(data, width, height, format)?;
+
+        let img = {
+            let width = shared_memory.width();
+            let height = shared_memory.height();
+            let format = shared_memory.format();
+
+            image_from_wayland(data, width, height, format)?
+        };
 
         screenshots.push((output_info, img));
 
         shared_memory.destroy();
-
-        let state = &mut *state_mutex.lock().unwrap();
-        state.next_screen(); // reset current screenshot metadata
+        manager.next_screen(); // reset current screenshot metadata
     }
 
     Ok(screenshots)
-}
-
-fn init_queue() -> anyhow::Result<(Arc<Mutex<EventQueue<WaylandScreenshotState>>>, QueueHandle<WaylandScreenshotState>)> {
-    let conn = Connection::connect_to_env()?;
-
-    let display = conn.display();
-
-    let queue_mutex: Arc<Mutex<EventQueue<WaylandScreenshotState>>> =
-        Arc::new(Mutex::new(conn.new_event_queue()));
-
-    let queue_handle = {
-        let queue = &mut *queue_mutex.lock().unwrap();
-
-        queue.handle()
-    };
-
-    // attach to globals
-    display.get_registry(&queue_handle, ());
-
-    Ok((queue_mutex, queue_handle))
 }
 
 // Transforms the buffer containing our image from the wayland compositor into a `image::DynamicImage`.
@@ -197,43 +111,4 @@ fn image_from_wayland(
     };
 
     Ok(result)
-}
-
-// This is an utility function that allows to asynchronously wait for an event.
-// We spawn a new thread that polls the queue using `poll_queue`.
-// Also this functions timeouts after 2 minutes when `poll_queue` does not return.
-async fn await_queue_events<T: 'static + Send>(
-    until: impl Fn(&T) -> bool + 'static + Send,
-    queue_mutex: Arc<Mutex<EventQueue<T>>>,
-    state_mutex: Arc<Mutex<T>>,
-) -> anyhow::Result<()> {
-    let timeout_result = tokio::time::timeout(
-        Duration::from_secs(120),
-        tokio::spawn(async { poll_queue(until, queue_mutex, state_mutex) }),
-    )
-        .await;
-
-    match timeout_result {
-        Ok(Ok(Ok(()))) => Ok(()),
-        Ok(Ok(Err(err))) => Err(WaylandError::EventQueuePollingError(err).into()),
-        Ok(Err(err)) => Err(WaylandError::ThreadSpawnFailed(err).into()),
-        Err(_) => Err(WaylandError::EventQueueTimeout.into()),
-    }
-}
-
-// polls queue until the function 'until' returns true.
-// Blocks the current thread
-fn poll_queue<T>(
-    until: impl Fn(&T) -> bool,
-    queue_mutex: Arc<Mutex<EventQueue<T>>>,
-    state_mutex: Arc<Mutex<T>>,
-) -> Result<(), DispatchError> {
-    let queue = &mut *queue_mutex.lock().unwrap();
-    let state = &mut *state_mutex.lock().unwrap();
-
-    while !until(state) {
-        queue.blocking_dispatch(state)?;
-    }
-
-    Ok(())
 }
