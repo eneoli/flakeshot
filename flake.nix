de92f3e40b1d0368be6763532b43a4636a55556c
TornaxO7 <tornax@proton.me>
2023-11-27 12:26:47 +0100

    flake: update build dependencies

--- flake.nix
+++ flake.nix
@@ -55,16 +55,19 @@
           pkgs.mkShell rec {
             packages = with pkgs; [
               pkg-config
-            ] ++ [ toolchain ];
-
-            buildInputs = with pkgs; [
+              patchelf
               gtk3
-            ];
+              pango
+              cairo
+              glib
+              gdk-pixbuf
+              libappindicator
+            ] ++ [ toolchain ];
 
             CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER = "${pkgs.llvmPackages.clangUseLLVM}/bin/clang";
             CARGO_ENCODED_RUSTFLAGS = "-Clink-arg=-fuse-ld=${pkgs.mold}/bin/mold";
 
-            LD_LIBRARY_PATH = "$LD_LIBRARY_PATH:${builtins.toString (pkgs.lib.makeLibraryPath buildInputs)} ";
+            LD_LIBRARY_PATH = "$LD_LIBRARY_PATH:${builtins.toString (pkgs.lib.makeLibraryPath packages)} ";
           };
       });
     };
--- src/tray.rs
+++ src/tray.rs
@@ -1,1 +1,10 @@
+use tray_icon::TrayIconBuilder;
 
+pub fn start() {
+    let tray_icon = TrayIconBuilder::new()
+        .with_tooltip("I use Arch btw.")
+        .build()
+        .unwrap();
+
+    loop {}
+}
