99b99ce32e5af30d814b3c994c414fc415f088e9
TornaxO7 <tornax@proton.me>
2024-01-08 19:44:48 +0100

    migrate to `flake-parts`

--- flake.nix
+++ flake.nix
@@ -2,71 +2,93 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = { nixpkgs, rust-overlay, ... }:
-    let
-      forAllSystems = function:
-        nixpkgs.lib.genAttrs [
-          "x86_64-linux"
-        ]
-          (system: function (import nixpkgs {
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+
+      perSystem = { self', lib, system, pkgs, config, ... }:
+        let
+
+          mkFlakeshot =
+            { rustPlatform
+            , lib
+            , pkg-config
+            , pango
+            , gdk-pixbuf
+            , gtk4
+            , gtk4-layer-shell
+            , libadwaita
+            , ...
+            }: rustPlatform.buildRustPackage.override
+              {
+                stdenv = pkgs.stdenvAdapters.useMoldLinker pkgs.clangStdenv;
+              }
+              rec {
+
+                nativeBuildInputs = [
+                  pkg-config
+                  pango
+                  gdk-pixbuf
+                ];
+
+                buildInputs = [
+                  gtk4
+                  gtk4-layer-shell
+                  libadwaita
+                ];
+
+                pname = "flakeshot";
+                version = "0.0.1";
+
+                src = builtins.path {
+                  path = ./.;
+                };
+
+                cargoLock.lockFile = ./Cargo.lock;
+
+                meta = {
+                  description = "A screenshot tool for wayland and x11!";
+                  homepage = "https://github.com/eneoli/flakeshot/";
+                  license = lib.licenses.gpl2;
+                  mainProgram = pname;
+                };
+              };
+        in
+        {
+          _module.args.pkgs = import inputs.nixpkgs {
             inherit system;
 
-            overlays = [ rust-overlay.overlays.default ];
-          }));
+            overlays = with inputs; [
+              rust-overlay.overlays.default
+            ];
+          };
 
-      mkFlakeshot = { rustPlatform, lib, ... }: rustPlatform.buildRustPackage {
-        pname = "flakeshot";
-        version = "0.0.1";
+          apps.default = {
+            type = "app";
+            program = lib.meta.getExe self'.packages.default;
+          };
 
-        src = ./.;
-        cargoLock.lockFile = ./Cargo.lock;
+          packages.default = pkgs.callPackage mkFlakeshot { };
 
-        meta = {
-          description = "A screenshot tool for wayland and x11!";
-          homepage = "https://github.com/eneoli/flakeshot/";
-          license = lib.licenses.gpl2;
+          devShells.default =
+            let
+              flakeshot = self'.packages.default;
+              rust-toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+            in
+            pkgs.mkShell.override
+              {
+                stdenv = pkgs.stdenvAdapters.useMoldLinker pkgs.clangStdenv;
+              }
+              {
+                packages = [ rust-toolchain ] ++ flakeshot.nativeBuildInputs ++ flakeshot.buildInputs;
+              };
         };
-      };
-    in
-    {
-      apps = forAllSystems
-        (pkgs:
-          let
-            flakeshotPkg = pkgs.callPackage mkFlakeshot { };
-          in
-          {
-            default = {
-              type = "app";
-              program = "${flakeshotPkg}/bin/flakeshot";
-            };
-          });
-
-      overlays.default = final: prev: {
-        flakeshot = prev.callPackage mkFlakeshot { };
-      };
-
-      devShells = forAllSystems (pkgs: {
-        default =
-          let
-            toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-          in
-          pkgs.mkShell.override
-            {
-              stdenv = pkgs.stdenvAdapters.useMoldLinker pkgs.clangStdenv;
-            }
-            {
-              packages = with pkgs; [
-                pkg-config
-                gtk4
-                gtk4-layer-shell
-                libadwaita
-                pango
-                gdk-pixbuf
-              ] ++ [ toolchain ];
-            };
-      });
     };
 }
 
