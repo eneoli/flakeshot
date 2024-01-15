0c0984cb5876e0b43a307046f8a44e7eedfbe367
TornaxO7 <tornax@proton.me>
2024-01-15 01:29:08 +0100

    adding subcommand

--- src/cli.rs
+++ src/cli.rs
@@ -1,6 +1,24 @@
 //! Contains the Cli implementation of flakeshot.
-use clap::Parser;
+use clap::{Parser, Subcommand};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about)]
-pub struct Cli {}
+pub struct Cli {
+    #[command(subcommand)]
+    command: Option<Command>,
+}
+
+impl Cli {
+    pub fn command(&self) -> Command {
+        self.command.unwrap_or(Command::Tray)
+    }
+}
+
+#[derive(Subcommand, Debug, Clone, Copy)]
+pub enum Command {
+    /// Open the manual capture ui
+    Gui,
+
+    /// Start the system tray of flakeshot. (default)
+    Tray,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,25 @@
 use clap::Parser;
-use flakeshot::{cli::Cli, tray};
+use flakeshot::cli::Command;
 use flakeshot::frontend::main_window::AppModel;
+use flakeshot::{cli::Cli, tray};
 use relm4::{
     gtk::{self, CssProvider},
     RelmApp,
 };
 
 fn main() {
-    let app = RelmApp::new("org.flakeshot.app");
-    relm4_icons::initialize_icons();
-    initialize_css();
+    let cli = Cli::parse();
 
-    app.run::<AppModel>(());
+    match cli.command() {
+        Command::Gui => {
+            let app = RelmApp::new("org.flakeshot.app");
+            relm4_icons::initialize_icons();
+            initialize_css();
+
+            app.run::<AppModel>(());
+        }
+        Command::Tray => tray::start(),
+    };
 }
 
 fn initialize_css() {
