c568ffcaf4012aabd7495705f748dac0176d5b93
eneoli <oliver@eneoli.de>
2024-01-24 22:37:42 +0100

    added ToolCommand

--- src/frontend/ui/tool/mod.rs
+++ src/frontend/ui/tool/mod.rs
@@ -1,18 +1,21 @@
-use std::{rc::Rc, cell::RefCell};
-
 use crate::frontend::screenshot_window::MouseEvent;
 
 use super::drawable::Drawable;
 
 pub mod crop;
 
-pub trait Tool {
-    fn handle_mouse_move(&mut self, x: f64, y: f64);
-    fn handle_mouse_press(&mut self, x: f64, y: f64);
-    fn handle_mouse_release(&mut self, x: f64, y: f64);
-    fn get_drawable(&self) -> Rc<RefCell<Box<dyn Drawable>>>;
+pub enum ToolCommand {
+    Nop,
+    Crop(f64, f64, f64, f64),
+}
 
-    fn handle_mouse_event(&mut self, event: MouseEvent) {
+pub trait Tool {
+    fn handle_mouse_move(&mut self, x: f64, y: f64) -> ToolCommand;
+    fn handle_mouse_press(&mut self, x: f64, y: f64) -> ToolCommand;
+    fn handle_mouse_release(&mut self, x: f64, y: f64) -> ToolCommand;
+    fn get_drawable(&self) -> &dyn Drawable;
+
+    fn handle_mouse_event(&mut self, event: MouseEvent) -> ToolCommand {
         match event {
             MouseEvent::MouseMove(x, y) => self.handle_mouse_move(x, y),
             MouseEvent::MosePress(_, x, y) => self.handle_mouse_press(x, y),
