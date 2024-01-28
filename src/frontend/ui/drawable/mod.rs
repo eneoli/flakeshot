9fa3eb858d9b0f00b3a9a24bf575c76430218cbb
eneoli <oliver@eneoli.de>
2024-01-28 02:01:37 +0100

    added draw_final

--- src/frontend/ui/drawable/mod.rs
+++ src/frontend/ui/drawable/mod.rs
@@ -2,4 +2,5 @@ use cairo::ImageSurface;
 
 pub trait Drawable {
     fn draw(&self, ctx: &cairo::Context, surface: &ImageSurface);
+    fn draw_final(&self, ctx: &cairo::Context, surface: &ImageSurface);
 }
