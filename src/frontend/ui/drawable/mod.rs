75c8b79be7ba1ae5e36043bcb6233d0eff40fde0
eneoli <oliver@eneoli.de>
2024-02-03 02:21:24 +0100

    added draw_active and draw_inactive

--- src/frontend/ui/drawable/mod.rs
+++ src/frontend/ui/drawable/mod.rs
@@ -1,6 +1,7 @@
 use cairo::ImageSurface;
 
 pub trait Drawable {
-    fn draw(&self, ctx: &cairo::Context, surface: &ImageSurface);
+    fn draw_active(&self, ctx: &cairo::Context, surface: &ImageSurface);
+    fn draw_inactive(&self, ctx: &cairo::Context, surface: &ImageSurface);
     fn draw_final(&self, ctx: &cairo::Context, surface: &ImageSurface);
 }
