4784b15e52999ec38c05f1ede365ca344945dbc7
TornaxO7 <tornax@proton.me>
2024-02-08 10:28:56 +0100

    tracing: adding panic hook to log panics as well

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,17 @@ use flakeshot::cli::Cli;
 use flakeshot::frontend::window::run_mode::RunMode;
 
 fn main() {
+    hook_tracing_for_panics();
     let cli = Cli::parse();
 
     flakeshot::init_logging(&cli.log_level, &cli.log_path);
     flakeshot::start(RunMode::from(cli.command()));
+}
+
+fn hook_tracing_for_panics() {
+    let prev_hook = std::panic::take_hook();
+    std::panic::set_hook(Box::new(move |panic_info| {
+        tracing_panic::panic_hook(panic_info);
+        prev_hook(panic_info);
+    }));
 }
