b337a14f02e8bc91b25ffca27433c42fc7efcff4
TornaxO7 <tornax@proton.me>
2024-02-09 12:48:02 +0100

    adding `Debug` implementation for `ui_manager`

--- src/frontend/ui/drawable/mod.rs
+++ src/frontend/ui/drawable/mod.rs
@@ -1,6 +1,6 @@
 use gtk4::cairo::{Context, ImageSurface};
 
-pub trait Drawable {
+pub trait Drawable: std::fmt::Debug {
     fn draw_active(&self, ctx: &Context, surface: &ImageSurface);
     fn draw_inactive(&self, ctx: &Context, surface: &ImageSurface);
     fn draw_final(&self, ctx: &Context, surface: &ImageSurface);
