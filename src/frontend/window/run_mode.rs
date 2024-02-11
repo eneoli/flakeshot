a33edb99c2b65c51ba05a77072db256a394bdcbf
TornaxO7 <tornax@proton.me>
2024-02-11 12:49:00 +0100

    run_mode: improve pattern matching

--- src/frontend/window/run_mode.rs
+++ src/frontend/window/run_mode.rs
@@ -11,7 +11,12 @@ impl From<Command> for RunMode {
         match value {
             Command::Gui => Self::Gui,
             Command::Tray => Self::Tray,
-            _ => unreachable!("There's  no run mode for {:?} defined.", value),
+            Command::PrintDefaultConfig => {
+                panic!(
+                    "There's no run mode for {:?} defined.",
+                    Command::PrintDefaultConfig
+                )
+            }
         }
     }
 }
